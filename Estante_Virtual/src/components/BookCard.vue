0b139760413660a2a41d6b60a6530ea6a4ae0280
LuizBuarqueDev <luizgabrielbuarque@gmail.com>
2024-10-21 16:53:35 -0300

    Mudança na forma que os livros sao carregados

--- Estante_Virtual/src/components/BookCard.vue
+++ Estante_Virtual/src/components/BookCard.vue
@@ -1,77 +1,75 @@
 <template>
-    <div v-if="books.length" class="book-container">
-        <div v-for="book in books" :key="book.id" class="book-item">
-            <span class="titulo">{{ book.title }}</span>
-            <img :src="book.image_link" alt="Capa do livro">
-        </div>
+  <div v-if="books.length" class="book-container">
+    <div v-for="book in books" :key="book.id" class="book-item">
+      <span class="title">{{ book.title }}</span>
+      <img :src="book.image_link || defaultImage" alt="Capa do livro">
     </div>
-    <div v-else>
-        <p>Nenhum livro disponível.</p>
-    </div>
+  </div>
+  <div v-else>
+    <p class="no-books-found">Nenhum livro disponível.</p>
+  </div>
 </template>
 
 <script>
-import { db } from '../assets/js/firebase';  // Importa a instância do Firestore
-import { collection, getDocs } from 'firebase/firestore';
-
 export default {
-    data() {
-        return {
-            books: [],
-        };
+  props: {
+    books: {
+      type: Array,
+      required: true,
     },
-    async created() {
-        try {
-            const querySnapshot = await getDocs(collection(db, "books"));  // Pega todos os livros da coleção "books"
-            querySnapshot.forEach((doc) => {
-                this.books.push({ id: doc.id, ...doc.data() });  // Adiciona os dados dos livros ao array "books"
-            });
-        } catch (error) {
-            console.error("Erro ao buscar livros do Firestore:", error);
-        }
-    },
+  },
+  data() {
+    return {
+      defaultImage: "/img/bookImg.png",
+    };
+  },
 };
 </script>
 
 <style scoped>
 .book-container {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
 
-    & .book-item {
-        position: relative;
-        margin: 20px;
-        border: 1px solid red;
-        border-radius: 8px;
-        overflow: hidden;
+  & .book-item {
+    position: relative;
+    margin: 20px;
+    border: 1px solid red;
+    border-radius: 8px;
+    overflow: hidden;
 
-        & .titulo {
-            font-size: 1.2em;
-            position: absolute;
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%, -50%);
-            color: var(--color_white);
-            opacity: 0;
-            transition: opacity 0.3s ease;
-            z-index: 1;
-        }
-
-        & img {
-            width: 180px;
-            height: 250px;
-            object-fit: cover;
-            transition: 0.3s;
-        }
-
-        &:hover .titulo {
-            opacity: 1;
-        }
-
-        &:hover img {
-            filter: brightness(40%) blur(2px);
-        }
+    & .title {
+      font-size: 1.2em;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      color: var(--color_white);
+      opacity: 0;
+      transition: opacity 0.3s ease;
+      z-index: 1;
     }
+
+    & img {
+      width: 180px;
+      height: 250px;
+      object-fit: cover;
+      transition: 0.3s;
+    }
+
+    &:hover .title {
+      opacity: 1;
+    }
+
+    &:hover img {
+      filter: brightness(40%) blur(2px);
+    }
+  }
+}
+
+.no-books-found {
+  margin: 20px;
+
 }
 </style>
--- Estante_Virtual/src/views/HomeView.vue
+++ Estante_Virtual/src/views/HomeView.vue
@@ -1,34 +1,62 @@
-<script setup>
-import BaseLayout from '@/components/BaseLayout.vue';
-import HeaderTemplate from '@/components/HeaderTemplate.vue';
-import BookCard from '@/components/BookCard.vue';
-</script>
-
 <template>
   <HeaderTemplate />
   <BaseLayout>
 
-    <div class="titulo-pratileira">
+    <div class="shelf-title">
       <i class="fa-solid fa-medal"></i>
       <span>Destaques</span>
     </div>
-    <section class="pratileira">
-      <BookCard />
+    <section class="shelf">
+      <BookCard :books="highlightedBooks" />
     </section>
 
-    <div class="titulo-pratileira">
+    <div class="shelf-title">
       <i class="fa-regular fa-font-awesome"></i>
-      <span> Descubra outros livros</span>
+      <span>Descubra outros livros</span>
     </div>
-    <section class="pratileira">
-      <BookCard />
+    <section class="shelf">
+      <BookCard :books="otherBooks" />
     </section>
 
   </BaseLayout>
 </template>
 
+<script>
+import { db } from '../assets/js/firebase';
+import { collection, getDocs } from 'firebase/firestore';
+import BaseLayout from '@/components/BaseLayout.vue';
+import HeaderTemplate from '@/components/HeaderTemplate.vue';
+import BookCard from '@/components/BookCard.vue';
+
+export default {
+  components: { BaseLayout, HeaderTemplate, BookCard },
+  data() {
+    return {
+      books: [],
+      highlightedBooks: [],
+      otherBooks: [],
+    };
+  },
+  async created() {
+    try {
+      const querySnapshot = await getDocs(collection(db, "books"));
+      querySnapshot.forEach((doc) => {
+        const book = { id: doc.id, ...doc.data() };
+        this.books.push(book);
+      });
+
+      this.highlightedBooks = this.books.filter(book => book.isHighlighted); // TUDO: Adicionar o atributo isHighlighted ao firebase
+      this.otherBooks = this.books.filter(book => !book.isHighlighted);
+
+    } catch (error) {
+      console.error("Erro ao buscar livros do Firestore:", error);
+    }
+  },
+};
+</script>
+
 <style scoped>
-.titulo-pratileira {
+.shelf-title {
   margin: 20px;
   color: var(--color_1) ;
 
