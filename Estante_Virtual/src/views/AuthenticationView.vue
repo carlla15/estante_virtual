199fe7678f00861b1d51efd8ddb83e86b39cc3f9
LuizBuarqueDev <luizgabrielbuarque@gmail.com>
2024-10-24 17:18:48 -0300

    Mudança na foro da pagina de login

--- Estante_Virtual/src/views/AuthenticationView.vue
+++ Estante_Virtual/src/views/AuthenticationView.vue
@@ -1,48 +1,56 @@
 <template>
     <BaseLayout>
         <section>
-            <div class="container-fluid h-custom">
+            <div class="container py-5 h-100">
                 <div class="row d-flex justify-content-center align-items-center h-100">
-                    <div class="col-md-9 col-lg-6 col-xl-5">
-                        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
-                            class="img-fluid" alt="Sample image">
-                    </div>
-                    <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
-                        <form>
-                            <!-- Email input -->
-                            <div data-mdb-input-init class="form-outline mb-4">
-                                <input type="email" id="form3Example3" class="form-control form-control-lg"
-                                    placeholder="Enter a valid email address" />
-                                <label class="form-label" for="form3Example3">Email address</label>
-                            </div>
-
-                            <!-- Password input -->
-                            <div data-mdb-input-init class="form-outline mb-3">
-                                <input type="password" id="form3Example4" class="form-control form-control-lg"
-                                    placeholder="Enter password" />
-                                <label class="form-label" for="form3Example4">Password</label>
-                            </div>
-
-                            <div class="d-flex justify-content-between align-items-center">
-                                <!-- Checkbox -->
-                                <div class="form-check mb-0">
-                                    <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3" />
-                                    <label class="form-check-label" for="form2Example3">
-                                        Remember me
-                                    </label>
+                    <div class="col col-xl-10">
+                        <div class="card" style="border-radius: 1rem;">
+                            <div class="row g-0">
+                                <div class="col-md-6 col-lg-5 d-none d-md-block">
+                                    <img src="/img/img_login.png"
+                                        alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem;" />
                                 </div>
-                                <a href="#!" class="text-body">Forgot password?</a>
-                            </div>
+                                <div class="col-md-6 col-lg-7 d-flex align-items-center">
+                                    <div class="card-body p-4 p-lg-5 text-black">
 
-                            <div class="text-center text-lg-start mt-4 pt-2">
-                                <button type="button" data-mdb-button-init data-mdb-ripple-init
-                                    class="btn btn-primary btn-lg"
-                                    style="padding-left: 2.5rem; padding-right: 2.5rem;">Login</button>
-                                <p class="small fw-bold mt-2 pt-1 mb-0">Don't have an account? <a href="#!"
-                                        class="link-danger">Register</a></p>
-                            </div>
+                                        <form>
 
-                        </form>
+                                            <div class="d-flex align-items-center mb-3 pb-1">
+                                                <i class="fas fa-cubes fa-2x me-3" style="color: #ff6219;"></i>
+                                                <span class="h1 fw-bold mb-0">Logo</span>
+                                            </div>
+
+                                            <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Sign into your
+                                                account</h5>
+
+                                            <div data-mdb-input-init class="form-outline mb-4">
+                                                <input type="email" id="form2Example17"
+                                                    class="form-control form-control-lg" />
+                                                <label class="form-label" for="form2Example17">Email address</label>
+                                            </div>
+
+                                            <div data-mdb-input-init class="form-outline mb-4">
+                                                <input type="password" id="form2Example27"
+                                                    class="form-control form-control-lg" />
+                                                <label class="form-label" for="form2Example27">Password</label>
+                                            </div>
+
+                                            <div class="pt-1 mb-4">
+                                                <button data-mdb-button-init data-mdb-ripple-init
+                                                    class="btn btn-dark btn-lg btn-block" type="button">Login</button>
+                                            </div>
+
+                                            <a class="small text-muted" href="#!">Forgot password?</a>
+                                            <p class="mb-5 pb-lg-2" style="color: #393f81;">Don't have an account? <a
+                                                    href="#!" style="color: #393f81;">Register here</a></p>
+                                            <a href="#!" class="small text-muted">Terms of use.</a>
+                                            <a href="#!" class="small text-muted">Privacy policy</a>
+                                        </form>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -60,23 +68,4 @@ export default {
 
 </script>
 
-<style scoped>
-
-.divider:after,
-.divider:before {
-    content: "";
-    flex: 1;
-    height: 1px;
-    background: #eee;
-}
-
-.h-custom {
-    box-shadow: 2px 1px 3px rgba(0, 0, 0, 0.644);
-    width: 90%;
-    border-radius: 20px;
-    padding: 80px;
-    background-color: var(--color_4);
-    margin-top: 50px;
-    margin-bottom: 50px;
-}
-</style>
+<style scoped></style>
