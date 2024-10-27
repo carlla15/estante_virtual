62635dfcc2b9a511b1bce5ffa1ca32c61b0c80ce
LuizBuarqueDev <luizgabrielbuarque@gmail.com>
2024-10-27 00:29:10 -0300

    Simplificaçao do componente

--- Estante_Virtual/src/views/AuthView.vue
+++ Estante_Virtual/src/views/AuthView.vue
@@ -2,22 +2,19 @@
 
     <BaseLayout>
 
-        <section>
-            <div class="container py-5 h-100">
-                <div class="row d-flex justify-content-center align-items-center h-100">
-                    <div class="col col-xl-10">
-                        <div class="card" style="border-radius: 1rem;">
-                            <div class="row g-0">
-                                <div class="col-md-6 col-lg-5 d-none d-md-block">
-                                    <img src="/img/img_login.png" alt="login form" class="img-fluid"
-                                        style="border-radius: 1rem 0 0 1rem;" />
-                                </div>
-                                <div class="col-md-6 col-lg-7 d-flex align-items-center">
-                                    <div class="card-body p-4 p-lg-5 text-black">
-                                        <AuthLogin v-show="isLogin" @switchToRegister="isLogin = false" key="login" />
-                                        <AuthRegister v-show="!isLogin" @switchToLogin="isLogin = true"
-                                            key="register" />
-                                    </div>
+        <section class="container py-5 h-100">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col col-xl-10">
+                    <div class="card" style="border-radius: 1rem;">
+                        <div class="row g-0">
+                            <div class="col-md-6 col-lg-5 d-none d-md-block">
+                                <img src="/img/img_login.png" alt="login form" class="img-fluid"
+                                    style="border-radius: 1rem 0 0 1rem;" />
+                            </div>
+                            <div class="col-md-6 col-lg-7 d-flex align-items-center">
+                                <div class="card-body p-4 p-lg-5 text-black">
+                                    <AuthLogin v-show="isLogin" @switchToRegister="isLogin = false" key="login" />
+                                    <AuthRegister v-show="!isLogin" @switchToLogin="isLogin = true" key="register" />
                                 </div>
                             </div>
                         </div>
