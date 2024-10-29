77bc9792b55880a91cda69b7faaf0bd7e7f84837
LuizBuarqueDev <luizgabrielbuarque@gmail.com>
2024-10-28 23:37:54 -0300

    Ajuste do Boostrap e CSS do componete Login

--- Estante_Virtual/src/components/auth/AuthLogin.vue
+++ Estante_Virtual/src/components/auth/AuthLogin.vue
@@ -1,37 +1,61 @@
 <template>
   <form>
-    <div class="d-flex align-items-center mb-3 pb-1">
+    <div>
       <i class="fa-solid fa-book-bookmark"></i>
-      <span class="h1 fw-bold mb-0">Pratileira Inteligente</span>
+      <span class="h1 fw-bold">Pratileira Inteligente</span>
     </div>
 
-    <h5 class="fw-normal mb-3 pb-3">Entre na sua conta</h5>
-    <div class="form-outline mb-4">
+    <h5>Entre na sua conta</h5>
+    <div>
       <input type="email" id="loginEmail" class="form-control form-control-lg" />
       <label class="form-label" for="loginEmail">Endereço de email</label>
     </div>
 
-    <div class="form-outline mb-4">
+    <div>
       <input type="password" id="loginPassword" class="form-control form-control-lg" />
       <label class="form-label" for="loginPassword">Senha</label>
     </div>
 
-    <div class="pt-1 mb-4">
+    <div>
       <button class="btn btn-dark btn-lg btn-block" type="button">Login</button>
     </div>
 
-    <a class="small text-muted" href="#!">Esqueceu sua senha ?</a>
-    <p class="mb-5 pb-lg-2" style="color: #393f81;">Não tem uma conta?
-      <a href="#!" style="color: #393f81;" @click="$emit('switchToRegister')">Registre-se aqui</a>
+    <a href="#!">Esqueceu sua senha ?</a>
+    <p>Não tem uma conta?
+      <a href="#" @click="$emit('switchToRegister')">Registre-se aqui</a>
     </p>
   </form>
 </template>
 
 <style scoped>
 form {
+
+  div {
+    margin-bottom: 16px;
+    padding-bottom: 4px;
+
+    & input {
+      border-color: gray;
+    }
+  }
+
   & i {
     font-size: 2em;
     margin-right: 10px
+  }
+
+  & a {
+    font-size: small;
+    color: var(--color_black);
+  }
+
+  & p {
+    margin-top: 16px;
+    color: #393f81;
+
+    & a {
+      color: #393f81
+    }
   }
 }
 </style>
