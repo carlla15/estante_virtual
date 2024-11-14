68a65a950712dd170bbe51eeb41bae43ad22f7bf
carlla15 <carllapatricia25@gmail.com>
2024-11-14 00:26:01 -0300

    colocando layout base na bookcase

---
+++ Estante_Virtual/src/views/BookcaseView.vue
@@ -0,0 +1,23 @@
+<template>
+    <BaseLayout>
+        <div class="conteiner-bookcase">
+
+
+
+            
+        </div>
+    
+    
+    
+    
+    
+    </BaseLayout>
+
+</template>
+
+<script setup>
+
+
+import BaseLayout from '@/components/BaseLayout.vue';
+
+</script>
