86424858172cdd4aaa929513c595ae97ad83add1
carlla15 <carllapatricia25@gmail.com>
2024-11-14 18:11:46 -0300

    seção de livros lidos na estante

--- Estante_Virtual/src/views/BookcaseView.vue
+++ Estante_Virtual/src/views/BookcaseView.vue
@@ -2,15 +2,23 @@
     <BaseLayout>
         <div class="conteiner-bookcase">
 
+            <div class="section-title">
+                <h2> <i class="fa-solid fa-circle-check"></i> Livros Lidos</h2>
 
+            </div>
+
+                <section class="book-grid">
+                    <div v-for="book in readBooks" :key="book.id" class="book-card">
+
+                    <img :src="book.coverUrl" alt="Capa do Livro" class="book-cover" />
+                    
+                    <p>{{ book.title }}</p>
+                    </div>
+                </section>
 
             
         </div>
-    
-    
-    
-    
-    
+  
     </BaseLayout>
 
 </template>
