4878ee76a648aedb1f7bd829d55eb6d9bf18eede
LuizBuarqueDev <luizgabrielbuarque@gmail.com>
2024-11-24 16:19:32 -0300

    Ajuste no BookDetail

--- Estante_Virtual/src/views/BookDetail.vue
+++ Estante_Virtual/src/views/BookDetail.vue
@@ -1,28 +1,28 @@
 <template>
   <BaseLayout>
-    <section class="container book-detail">
-      <div class="row">
-        <div class="col-md-4">
-          <img :src="bookData.image_link !== 'N/A' ? bookData.image_link : defaultImage" alt="Capa do livro"
-            class="book-image img-fluid" />
-        </div>
-        <div class="col-md-8">
-          <h2>{{ bookData.title || 'Título não disponível' }}</h2>
-          <p><strong>Autor(es):</strong> {{ bookData.authors?.join(', ') || 'N/A' }}</p>
-          <p><strong>Categoria(s):</strong> {{ bookData.categories?.join(', ') || 'N/A' }}</p>
-          <p><strong>Descrição:</strong> {{ bookData.description || 'N/A' }}</p>
-          <p><strong>Páginas:</strong> {{ bookData.page_count || 'N/A' }}</p>
-          <p><strong>Data de Publicação:</strong> {{ bookData.published_date || 'N/A' }}</p>
-          <p><strong>Editora:</strong> {{ bookData.publisher || 'N/A' }}</p>
-          <p><strong>Idioma:</strong> {{ bookData.language || 'N/A' }}</p>
-        </div>
+    <section class="row book-detail">
+
+      <aside class="col-md-4">
+        <img :src="bookData.image_link !== 'N/A' ? bookData.image_link : defaultImage" alt="Capa do livro"
+          class="book-image" />
+      </aside>
+
+      <div class="col-md-8">
+        <h2>{{ bookData.title || 'Título não disponível' }}</h2>
+        <p><strong>Autor(es):</strong> {{ bookData.authors?.join(', ') || 'N/A' }}</p>
+        <p><strong>Categoria(s):</strong> {{ bookData.categories?.join(', ') || 'N/A' }}</p>
+        <p><strong>Descrição:</strong> {{ bookData.description || 'N/A' }}</p>
+        <p><strong>Páginas:</strong> {{ bookData.page_count || 'N/A' }}</p>
+        <p><strong>Data de Publicação:</strong> {{ bookData.published_date || 'N/A' }}</p>
+        <p><strong>Editora:</strong> {{ bookData.publisher || 'N/A' }}</p>
+        <p><strong>Idioma:</strong> {{ bookData.language || 'N/A' }}</p>
       </div>
     </section>
   </BaseLayout>
 </template>
 
 <script setup>
-import { computed } from 'vue';
+import { computed, onMounted } from 'vue';
 import { useRoute } from 'vue-router';
 import BaseLayout from '@/components/BaseLayout.vue';
 
@@ -37,19 +37,31 @@ const bookData = computed(() => {
     return {};
   }
 });
+
+onMounted(() => {
+  window.scrollTo(0, 0);
+});
 </script>
 
 <style scoped>
 .book-detail {
   margin: 50px;
 
+  aside {
+    text-align: center;
+  }
+
+  p {
+    margin-bottom: 20px;
+  }
+
   .book-image {
-    max-width: 100%;
+    height: 500px;
+    width: 350px;
     border-radius: 8px;
     border: 2px solid red;
     overflow: hidden;
     box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
   }
-
 }
 </style>
