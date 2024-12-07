3fadad4b37a0c46c34108346b48516865bdb1181
Luiz_Buarque_Dev <luizgabrielbuarque@gmail.com>
2024-12-06 22:55:21 -0300

    Ajuste no css do botão do usuario

--- Estante_Virtual/src/components/UserTag.vue
+++ Estante_Virtual/src/components/UserTag.vue
@@ -8,43 +8,43 @@ const isAuthenticated = ref(false);
 
 // Função para fazer logout
 const logout = async () => {
-  await signOut(auth);
-  isAuthenticated.value = false;
-  userName.value = '';
+    await signOut(auth);
+    isAuthenticated.value = false;
+    userName.value = '';
 }
 
 onAuthStateChanged(auth, (user) => {
-  if (user) {
-    userName.value = user.email;
-    isAuthenticated.value = true;
-  } else {
-    isAuthenticated.value = false;
-    userName.value = '';
-  }
+    if (user) {
+        userName.value = user.email;
+        isAuthenticated.value = true;
+    } else {
+        isAuthenticated.value = false;
+        userName.value = '';
+    }
 });
 </script>
 
 <template>
-  <div v-if="!isAuthenticated">
-    <router-link class="nav-link" to="/authentication">
-      <i class="fa-solid fa-right-to-bracket"></i> Entre
-    </router-link>
-  </div>
-
-  <div v-else>
-    <div class="dropdown">
-      <a class="btn nav-link" type="button" data-bs-toggle="dropdown">
-        <i class="fa-regular fa-user"></i> {{ userName }}
-      </a>
-      <ul class="dropdown-menu">
-        <li>
-          <button @click="logout()" class="dropdown-item">
-            <i class="fa-solid fa-sign-out-alt"></i> Sair
-          </button>
-        </li>
-      </ul>
+    <div v-if="!isAuthenticated">
+        <router-link class="nav-link" to="/authentication">
+            <i class="fa-solid fa-right-to-bracket"></i> Entre
+        </router-link>
     </div>
-  </div>
+
+    <div v-else>
+        <div class="dropdown">
+            <a class="btn nav-link" type="button" data-bs-toggle="dropdown">
+                <i class="fa-regular fa-user"></i> {{ userName }}
+            </a>
+            <ul class="dropdown-menu">
+                <li>
+                    <button @click="logout()" class="dropdown-item">
+                        <i class="fa-solid fa-sign-out-alt"></i> Sair
+                    </button>
+                </li>
+            </ul>
+        </div>
+    </div>
 </template>
 
 <style scoped>
@@ -59,6 +59,11 @@ a {
         color: var(--color_white);
         background-color: var(--color_1_hover);
         transform: scale(1.1);
+    }
+
+    i {
+        margin-right: 10px;
+        font-size: 1.5em;
     }
 }
 </style>
