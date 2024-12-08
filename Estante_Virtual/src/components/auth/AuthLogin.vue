0fff63e081f28d0c5221d28d309ac7c38480a2b5
carlla15 <carllapatricia25@gmail.com>
2024-12-08 13:04:55 -0300

    colocando script setup no topo

--- Estante_Virtual/src/components/auth/AuthLogin.vue
+++ Estante_Virtual/src/components/auth/AuthLogin.vue
@@ -1,33 +1,3 @@
-<template>
-  
-  <form>
-    <div>
-      <i class="fa-solid fa-book-bookmark"></i>
-      <span class="h1 fw-bold">Prateleira Inteligente</span>
-    </div>
-
-    <h5>Entre na sua conta</h5>
-    <div>
-      <input type="email" v-model="loginData.email" class="form-control form-control-lg"/>
-      <label class="form-label" for="loginEmail">Endereço de email</label>
-    </div>
-
-    <div>
-      <input type="password" v-model="loginData.password" class="form-control form-control-lg" />
-      <label class="form-label" for="loginPassword">Senha</label>
-    </div>
-
-    <div>
-      <button @click="loginUser" class="btn btn-dark btn-lg btn-block" type="button">Login</button>
-    </div>
-
-    <a href="#!">Esqueceu sua senha ?</a>
-    <p>Não tem uma conta?
-      <a href="#" @click="$emit('switchToRegister')">Registre-se aqui</a>
-    </p>
-  </form>
-</template>
-
 <script setup>
 
 import { ref } from 'vue';
@@ -57,6 +27,37 @@ const loginUser = async () => {
 };
 
 </script>
+
+
+<template>
+  
+  <form>
+    <div>
+      <i class="fa-solid fa-book-bookmark"></i>
+      <span class="h1 fw-bold">Prateleira Inteligente</span>
+    </div>
+
+    <h5>Entre na sua conta</h5>
+    <div>
+      <input type="email" v-model="loginData.email" class="form-control form-control-lg"/>
+      <label class="form-label" for="loginEmail">Endereço de email</label>
+    </div>
+
+    <div>
+      <input type="password" v-model="loginData.password" class="form-control form-control-lg" />
+      <label class="form-label" for="loginPassword">Senha</label>
+    </div>
+
+    <div>
+      <button @click="loginUser" class="btn btn-dark btn-lg btn-block" type="button">Login</button>
+    </div>
+
+    <a href="#!">Esqueceu sua senha ?</a>
+    <p>Não tem uma conta?
+      <a href="#" @click="$emit('switchToRegister')">Registre-se aqui</a>
+    </p>
+  </form>
+</template>
 
 <style scoped>
 form {
