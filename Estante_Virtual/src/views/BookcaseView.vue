42eeb8ff975fc61912ffd25e58d3c46ec48c3e69
Luiz_Buarque_Dev <luizgabrielbuarque@gmail.com>
2024-12-09 13:11:40 -0300

    Link da pagina de cadastro de livros

--- Estante_Virtual/src/views/BookcaseView.vue
+++ Estante_Virtual/src/views/BookcaseView.vue
@@ -13,10 +13,10 @@ const bookService = new DAOService('books');
 const userBooks = ref([]);
 
 const fetchUserBooks = async () => {
-    const currentUser = auth.currentUser;
+    const user = auth.currentUser;
 
-    if (currentUser) {
-        const userBooksEntries = await userBooksService.search('uid', currentUser.uid);
+    if (user) {
+        const userBooksEntries = await userBooksService.search('uid', user.uid);
         // Extração dos IDs dos livros
         const bookIds = userBooksEntries.map(entry => entry.book);
         // Busca os detalhes de cada livro associado
@@ -27,13 +27,17 @@ onMounted(fetchUserBooks);
 </script>
 
 
-
 <template>
     <BaseLayout>
         <div class="container-bookcase">
             <div class="section-title">
-                <h2><i class="fa-solid fa-flag"></i> Livros na Estante</h2>
+                <h2><i class="fa-solid fa-flag"></i>Livros na Estante</h2>
             </div>
+            <RouterLink to="/bookregistration">
+                <button class="btn">
+                    <i class="fa-solid fa-plus"></i> Cadastrar livro
+                </button>
+            </RouterLink>
             <section class="book-grid d-flex">
                 <BooksCards :books="userBooks" />
             </section>
@@ -45,38 +49,25 @@ onMounted(fetchUserBooks);
 <style scoped>
 .container-bookcase {
     padding: 20px;
-}
 
-.section-title {
-    padding: 20px 0;
-    color: var(--color_4);
-    display: flex;
-    align-items: center;
-    font-size: 1.5em;
-}
+    .section-title {
+        padding: 20px 0;
+        color: var(--color_4);
+        display: flex;
+        align-items: center;
+        font-size: 1.5em;
 
-.section-title i {
-    margin-right: 10px;
-}
+        i {
+            margin-right: 10px;
+            color: var(--color_4);
+        }
+    }
 
-.book-grid {
-    display: grid;
-    grid-template-columns: repeat(4, 1fr);
-    gap: 20px;
-    padding-bottom: 40px;
-}
-
-.book-card {
-    text-align: center;
-    background: var(--color_2);
-    padding: 10px;
-    border-radius: 8px;
-}
-
-.book-cover {
-    width: 100%;
-    height: auto;
-    max-height: 200px;
-    border-radius: 5px;
+    .book-grid {
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+        gap: 20px;
+        padding-bottom: 40px;
+    }
 }
 </style>
